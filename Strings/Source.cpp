c6d1aa84009e8f87bd0e340cbe3da94e6dba2570
Stridin <32247758+Stridin@users.noreply.github.com>
2017-10-23 22:42:29 +0200

    c-string!

--- Strings/Source.cpp
+++ Strings/Source.cpp
@@ -1,4 +1,18 @@
+#include <conio.h>
+
+void print( const char* s )
+{
+	for ( ; *s != 0; s++ )
+	{
+		_putch( *s );
+	}
+}
+
 int main()
 {
+	const char msg[] = { 'P', 'u', 'b', 'e', 's', '!', 0 };
+	print( msg );
+
+	while ( !_kbhit() );
 	return 0;
 }
